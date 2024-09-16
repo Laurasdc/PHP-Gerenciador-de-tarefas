d50e2c01103ecc6e8b8d104949ea995e1cd52f1a
Laura Souza de Carvalho <laurascarvalho@prefeitura.sp.gov.br>
2024-09-16 18:00:22 -0300

    Finalização Capítulo 12

---
+++ tarefas/tarefa.php
@@ -0,0 +1,48 @@
+<?php
+
+include "banco.php";
+include "ajudantes.php";
+
+$tarefa = buscar_tarefa($conexao, $_GET['id']);
+
+if (! is_array($tarefa)) {
+    http_response_code(404);
+    echo "Tarefa não encontrada.";
+    die();
+}
+
+$tem_erros = false;
+$erros_validacao = array();
+
+if (tem_post()) {
+    $tarefa_id = $_POST['tarefa_id'];
+
+    if(! array_key_exists('anexo', $_FILES)) {
+        $tem_erros = true;
+        $erros_validacao['anexo'] =
+            'Você deve selecionar um arquivo para anexar';
+    } else {
+        if (tratar_anexo($_FILES['anexo'])) {
+            $nome = $_FILES['anexo']['name'];
+            $anexo = [
+                'tarefa_id' => $tarefa_id,
+                'nome' => substr($nome, 0, -4),
+                'arquivo' => $nome,
+            ];
+        } else {
+            $tem_erros = true;
+            $erros_validacao['anexo'] =
+                'Envie anexos nos formatos zip ou pdf';
+        }
+    }
+
+    if(! $tem_erros) {
+        gravar_anexo($conexao, $anexo);
+    }
+
+}
+
+$tarefa = buscar_tarefa($conexao, $_GET['id']);
+$anexos = buscar_anexos($conexao, $_GET['id']);
+
+include "template_tarefa.php";
